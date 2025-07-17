14b22bc8a100edc562953067fc7eff45a1bb79aa
52443 <52443@DAYANA>
2025-07-16 20:52:26 -0600

    se elimno el dash de andres pq esta feo, hare uno nuevo

--- src/fix_care/event/EventMenuSelected.java
+++
@@ -1,6 +0,0 @@
-package fix_care.event;
-
-public interface EventMenuSelected {
-
-    public void selected(int index);
-}
--- src/fix_care/model/StatusType.java
+++
@@ -1,5 +0,0 @@
-package fix_care.model;
-
-public enum StatusType {
-    PENDING, APPROVED, REJECT
-}
